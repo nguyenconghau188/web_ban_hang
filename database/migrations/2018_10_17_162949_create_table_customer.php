68b76283b2ca9ff46a64e8f24b064afd348345cc
nguyenconghau <nguyenconghau188@gmail.com>
2018-10-18 00:31:18 +0700

    migration: create table users

--- database/migrations/2018_10_17_162949_create_table_customer.php
+++ database/migrations/2018_10_17_162949_create_table_customer.php
@@ -18,7 +18,7 @@ class CreateTableCustomer extends Migration
             $table->string('name', 100)->nullable();
             $table->string('gender', 10)->nullable();
             $table->string('email', 50)->nullable()->unique();
-            $table->string('password',72);
+            $table->string('password',255);
             $table->string('address', 100)->nullable();
             $table->string('phone_number', 20)->nullable();
             $table->string('note', 200)->nullable();
--- database/migrations/2018_10_18_002736_create_table_users.php
+++ database/migrations/2018_10_18_002736_create_table_users.php
@@ -4,20 +4,20 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTableUsers extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
+        public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('full_name', 255)->nullable();
+            $table->string('email', 255)->unique();
+            $table->string('password', 255);
             $table->rememberToken();
             $table->timestamps();
         });
