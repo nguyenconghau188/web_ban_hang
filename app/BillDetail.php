668f3a1fad761b329b22f5456dbd413dc127737f
nguyenconghau <nguyenconghau188@gmail.com>
2018-10-19 10:28:26 +0700

    commit missing

--- app/BillDetail.php
+++ app/BillDetail.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class BillDetail extends Model
 {
-    //
+    protected $table = 'bill_details';
+
+    public function bill()
+    {
+    	return $this->belongsTo('App\Bill', 'id_bill', 'id');
+    }
+
+    public function product()
+    {
+    	return $this->belongsTo('App\Product', 'id_product', 'id');
+    }
 }
